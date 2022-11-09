ab998270942093fb5619ebfac9310bc957c5832a
smerch88 <maxarmyk@gmail.com>
2022-11-10 01:58:42 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+let savedSettings = localStorage.getItem("videoplayer-current-time");
+let seconds = savedSettings;
+
+console.log("aaaa", seconds);
+
+player.on("play", function () {
+  console.log("played the video!");
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log("title:", title);
+});
+
+const onPlay = function (data) {
+  console.log(data);
+  // data is an object containing properties specific to that event
+};
+
+player.on("play", onPlay);
+
+player.on("play", function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem("videoplayer-current-time", data.seconds);
+});
+
+player.setCurrentTime(seconds);
